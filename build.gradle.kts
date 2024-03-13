efdc3c40916f5ab664183190519af701face77aa
Jesse <yesid_android@proton.me>
2024-03-13 12:42:34 -0500

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id ("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id ("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+    id ("io.gitlab.arturbosch.detekt") version "1.23.0"
 }
